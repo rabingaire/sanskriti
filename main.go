4f8ef17766118109699b0acc4830777138e737be
Rabin Gaire <rabingaire20@gmail.com>
2017-08-13 23:26:01 +0545

    repl is now added to language

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"./repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! Welcome to Sanskriti Programming Language build by @rabingaire! \n", user.Username)
+	fmt.Printf("To Exit type ^C Thank You! \n")
+	repl.Start(os.Stdin, os.Stdout)
+}
