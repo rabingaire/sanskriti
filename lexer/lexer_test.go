eb2e59f73906ffaf6774734491fb7d038656fb45
Rabin Gaire <rabingaire20@gmail.com>
2017-08-13 23:02:02 +0545

    now sanskriti supports == and !=

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,6 +21,8 @@ func TestNextToken(t *testing.T) {
 	} else {
 		return false;
 	}
+	10 == 10;
+	9 != 10;
 	`
 
 	tests := []struct {
@@ -92,6 +94,14 @@ func TestNextToken(t *testing.T) {
 		{ token.FALSE, "false" },
 		{ token.SEMICOLON, ";" },
 		{ token.RBRACE, "}" },
+		{ token.INT, "10" },
+		{ token.EQ, "==" },
+		{ token.INT, "10" },
+		{ token.SEMICOLON, ";" },
+		{ token.INT, "9" },
+		{ token.NOT_EQ, "!=" },
+		{ token.INT, "10" },
+		{ token.SEMICOLON, ";" },
 		{ token.EOF, "" },
 	}
 
