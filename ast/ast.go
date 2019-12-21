148614420e3767b9651a9c922e92ae94d00eb5d1
Rabin Gaire <rabingaire20@gmail.com>
2019-12-21 14:53:31 +0545

    Sanskriti can now parse let and return statements

---
+++ ast/ast.go
@@ -0,0 +1,81 @@
+package ast
+
+import (
+	"github.com/rabingaire/sanskriti/token"
+)
+
+// Node ...
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement ...
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression ...
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program ...
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral ...
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatement ...
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+// TokenLiteral ...
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier ...
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+// TokenLiteral ...
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+// ReturnStatement ...
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {
+
+}
+
+// TokenLiteral ...
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
